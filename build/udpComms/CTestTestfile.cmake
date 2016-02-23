1ab5f374d1542b410c108c4c964c7857c27c375b
aransena <senaa@tcd.ie>
2016-02-23 14:49:28 +0000

    cleanup

--- build/udpComms/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/clm/Aran/catkin_ws/src/udpComms
-# Build directory: /home/clm/Aran/catkin_ws/build/udpComms
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
