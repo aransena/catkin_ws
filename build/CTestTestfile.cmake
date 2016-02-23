1a6d48ea898967a6f90302063d39df73819ab6f3
aransena <senaa@tcd.ie>
2016-02-23 14:53:38 +0000

    cleanup

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/clm/Aran/catkin_ws/src
-# Build directory: /home/clm/Aran/catkin_ws/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(network_comms)
-SUBDIRS(nri)
